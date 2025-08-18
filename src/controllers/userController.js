1504b44a4700140c102433ba26f727d64e34c687
jessicalira1001 <jessicalira1001@gmail.com>
2025-08-18 20:35:45 -0300

    feat: Adiciona a criptografia da senha do usuário

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,11 @@
+const bcrypt = require('bcrypt')
 const {createUser} = require('../models/userModel')
 
 const register = async (req, res) =>{
     try{
         const{name, email, password} = req.body;
-        const newUser = await createUser(name, email, password);
+        const senhacriptografada = await bcrypt.hash(password, 10)
+        const newUser = await createUser(name, email, senhacriptografada);
         res.status(201).json(newUser);
     }catch (error){
         res.status(500).json({message: "Erro interno no servidor"})
